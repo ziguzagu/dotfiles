1545e85848c94c6bed413e2d7078e03721b6e00c
Hiroshi Sakai <ziguzagu@gmail.com>
2006-12-14 15:24:28 +0000

    r47@colinux:  ziguzagu | 2006-12-15 00:21:23 +0900  the path of working directory resolved dynamic.

--- dotlinks.pl
+++ dotlinks.pl
@@ -5,11 +5,14 @@ use strict;
 use warnings;
 
 use File::Basename;
+use FindBin;
 
 my %none_dot = map { $_ => 1 } qw(bin);
 my %except   = map { $_ => 1 } (basename $0);
 my @targets  = glob "*";
 
+my $dir = $FindBin::Bin;
+   $dir =~ s{^$ENV{HOME}/}{};
 chdir $ENV{HOME};
 for my $file (@targets) {
     next if $except{$file};
@@ -17,7 +20,7 @@ for my $file (@targets) {
     my $link = $none_dot{$file} ? $file : ".$file";
     next if -e $link or -l $link;
 
-    symlink "svn/dots/$file", "$link"
+    symlink "$dir/$file", "$link"
         or die "cannot make symlink: $!";
     print $link, "\n";
 }
