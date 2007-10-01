bd371cf294f11ec42bf4d37f8dfedda79d849bb7
Hiroshi Sakai <ziguzagu@gmail.com>
2007-10-01 16:21:11 +0000

    changed the way to link to dotfiles.

---
+++ dotlink.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CMD=`basename $0`
+DOTDIR="$PWD"
+
+DIRS=`find ./ -type d | sed -e 's/^\.\///;/^$/d;'`
+FILES=`find ./ -type f | sed -e 's/^\.\///;/^$/d;'`
+FILES=${FILES/$CMD/}
+
+cd $HOME
+for D in $DIRS; do
+    D=`echo $D | sed -e 's/^dot//'`
+    if [ ! -d "$D" ]; then
+        mkdir "$D"
+        echo "mkdir $D"
+    fi
+done
+
+for F in $FILES; do
+    TARGET=`echo $F | sed -e 's/^dot//'`
+    if [ ! -L "$TARGET" ]; then
+        ln -s "$DOTDIR/$F" "$TARGET"
+        echo "ln -s $DOTDIR/$F $TARGET"
+    fi
+done
+
+exit 0
