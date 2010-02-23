efe9396d429d72450092e7611dd28d905d1d14ab
Hiroshi Sakai <ziguzagu@gmail.com>
2010-02-23 12:55:35 +0000

    linebreaks

--- dot.emacs.d/init-color.el
+++ dot.emacs.d/init-color.el
@@ -1,13 +1,13 @@
 ;;;; -*- mode: lisp-interaction; syntax: elisp; coding: iso-2022-7bit -*-
 
-(provide 'init-color)
-
 (require 'font-lock)
 (global-font-lock-mode t)
+
 ;; basics
 (set-face-foreground 'default "white")
 (set-face-background 'default "black")
 (set-cursor-color "green")
+
 ;; common syntax
 (set-face-foreground 'font-lock-comment-face "green")
 (set-face-foreground 'font-lock-string-face  "red")
@@ -18,20 +18,25 @@
 (set-face-background 'font-lock-warning-face "red")
 (set-face-foreground 'font-lock-warning-face "white")
 (set-face-foreground 'font-lock-builtin-face "yellow")
+
 ;; highlight
 (set-face-background 'highlight "yellow")
 (set-face-foreground 'highlight "black")
+
 ;; region
 (set-face-background 'region "gray")
 (set-face-foreground 'region "black")
+
 ;; modeline
 (set-face-foreground 'modeline "black")
 (set-face-background 'modeline "cyan")
 (set-face-foreground 'mode-line-buffer-id "white")
 (set-face-foreground 'mode-line-inactive "black")
 (set-face-background 'mode-line-inactive "brightblack")
+
 ;; mini buffer
 (set-face-foreground 'minibuffer-prompt "yellow")
+
 ;; diff-mode
 (custom-set-faces
  '(diff-file-header ((t (:background "brightblack" :foreground "white"))))
@@ -39,5 +44,6 @@
  '(diff-context     ((t (:inherit shadow :foreground "white"))))
  '(diff-changed     ((t (:background "yellow" :foreground "black"))))
  '(diff-added       ((t (:background "green" :foreground "black"))))
- '(diff-removed     ((t (:background "brightred" :foreground "black"))))
- )
+ '(diff-removed     ((t (:background "brightred" :foreground "black")))))
+
+(provide 'init-color)
