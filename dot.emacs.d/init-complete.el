175f3ddbbd0e078d05aef56a8d3bf3c7d67d0820
Hiroshi Sakai <ziguzagu@gmail.com>
2012-03-08 17:59:17 +0900

    cleanup

--- dot.emacs.d/init-complete.el
+++ dot.emacs.d/init-complete.el
@@ -21,8 +21,7 @@
 (global-set-key "\C-o" 'dabbrev-expand)
 
 ;; yasnippet
-(add-to-list 'load-path
-             "~/.emacs.d/elisp/yasnippet")
+(add-to-list 'load-path "~/.emacs.d/elisp/yasnippet")
 (require 'yasnippet)
 (yas/load-directory "~/.emacs.d/snippets")
 (yas/global-mode 1)
