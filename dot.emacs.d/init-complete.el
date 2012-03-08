054a0f5b1acba9f157e25087d760f062d5f414e0
Hiroshi Sakai <ziguzagu@gmail.com>
2012-03-08 17:55:46 +0900

    Enabled dabbrev again.

--- dot.emacs.d/init-complete.el
+++ dot.emacs.d/init-complete.el
@@ -13,15 +13,12 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete-dict")
 (ac-config-default)
 
-;; temporary disabled
-;; ;; dabbrev
-;; ;;   * http://www.namazu.org/~tsuchiya/elisp/dabbrev-ja.el
-;; ;;   * http://www.namazu.org/~tsuchiya/elisp/dabbrev-highlight.el
-;; (load "dabbrev-ja")
-;; (require 'dabbrev-highlight)
-;; (setq dabbrev-case-fold-search t)
-;; (setq dabbrev-case-replace nil)
-;; (global-set-key "\C-o" 'dabbrev-expand)
+;; dabbrev
+(auto-install-from-url "http://www.namazu.org/~tsuchiya/elisp/dabbrev-highlight.el")
+(require 'dabbrev-highlight)
+(setq dabbrev-case-fold-search t)
+(setq dabbrev-case-replace nil)
+(global-set-key "\C-o" 'dabbrev-expand)
 
 ;; yasnippet
 (add-to-list 'load-path
