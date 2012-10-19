2f3868d43a7baeb09875ebb2eeff8ff93a717b6c
Hiroshi Sakai <ziguzagu@gmail.com>
2012-10-19 12:48:02 +0900

    typo

--- dot.emacs.d/init-anything.el
+++ dot.emacs.d/init-anything.el
@@ -13,7 +13,7 @@
                            anything-c-source-recentf
                            anything-c-source-files-in-current-dir+)
                          "*my-switch-buffer*"))
-(global-set-key (kbd "C-c ;") 'my-anything)
+(global-set-key (kbd "C-c ;") 'my-swithc-buffer)
 
 ;; woman
 (defun my-woman ()
