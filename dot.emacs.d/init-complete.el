4c941e38a307b4300d4b5a82e49ad87a1e08b3ed
Hiroshi Sakai <ziguzagu@gmail.com>
2012-11-05 17:43:37 +0900

    Improved zlc key map.

--- dot.emacs.d/init-complete.el
+++ dot.emacs.d/init-complete.el
@@ -22,3 +22,12 @@
 ;; zlc
 (el-get 'sync 'zlc)
 (require 'zlc)
+(setq zlc-select-completion-immediately t)
+(let ((map minibuffer-local-map))
+  (define-key map (kbd "<backtab>") 'zlc-select-previous)
+  (define-key map (kbd "S-<tab>") 'zlc-select-previous)
+  (define-key map (kbd "C-p") 'zlc-select-previous-vertical)
+  (define-key map (kbd "C-n") 'zlc-select-next-vertical)
+  (define-key map (kbd "C-b") 'zlc-select-previous)
+  (define-key map (kbd "C-f") 'zlc-select-next)
+  )
