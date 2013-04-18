14c3b3f7fab1bfecd761cc7ba6d48ac41d887b99
ziguzagu <ziguzagu@gmail.com>
2013-04-18 16:33:52 +0900

    cleanup init.el

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,25 +1,27 @@
-;;;; -*- mode: lisp-interaction; syntax: elisp; coding: utf-8 -*-
+;; -*- mode: lisp-interaction; syntax: elisp; coding: utf-8 -*-
 
-;;;;;; basics
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; basics
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (cd "~")
-(setq load-path
-      (append (list "~/.emacs.d"
-                    "~/.emacs.d/elisp") load-path))
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
 ;; using unsafe local variables..?
 (setq safe-local-variable-values (quote ((syntax . elisp))))
 
-;;;;;; profile
+;; profile
 (setq user-full-name "Hiroshi Sakai")
 (setq user-mail-address "ziguzagu@gmail.com")
 
-;;;;;; languages
+;; languages
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 (set-language-environment "Japanese")
 (set-default-coding-systems 'utf-8)
 
-;;;;;; emacs server/client on tmux
+;; emacs server/client on tmux
 (add-hook 'after-init-hook 'server-start)
 (if (getenv "TMUX")
     (shell-command "tmux display -p '#I' > ~/.emacs.d/emacs-server-window"))
@@ -29,8 +31,9 @@
              "rm ~/.emacs.d/emacs-server-window")))
 (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
 
-;;;;;; el-get
-;; https://github.com/dimitri/el-get
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; el-get
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 ;; install el-get
 (unless (require 'el-get nil t)
@@ -63,21 +66,21 @@
 ;; download
 (el-get 'sync)
 
-;;;;;; environment
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; initialize 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (load "init-appearance")
 (load "init-minibuffer")
 (load "init-scratch")
 (load "init-dired")
 (load "init-shell")
 
-;;;;;; generic editor functions
 (load "init-general")
 (load "init-complete")
 (load "init-anything")
 (load "init-migemo")
 (load "init-sdic")
 
-;;;;;; programming lang
 (load "init-cpp")
 (load "init-perl")
 (load "init-html")
