33e57873f4f260b04287d99bfd6a6dc04f5b9446
ziguzagu <ziguzagu@gmail.com>
2013-05-08 11:24:05 +0900

    typo

--- .emacs.d/init-html.el
+++ .emacs.d/init-html.el
@@ -18,7 +18,7 @@
 (el-get 'sync 'js2-mode)
 (autoload 'js2-mode "js2-mode" nil t)
 (setq-default js2-basic-offset 2)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 ;;;;;; utilities
 (defun escape-html-region (start end)
