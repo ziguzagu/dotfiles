a8b49729ff6bb9543599db76b6ffb30d86eb45c7
ziguzagu <ziguzagu@gmail.com>
2013-06-11 17:15:03 +0900

    update installing el-get code.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -35,14 +35,14 @@
 ;; el-get
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-;; install el-get
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (let (el-get-master-branch)
-       (goto-char (point-max))
-       (eval-print-last-sexp)))))
+;; install el-get and use master branch
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 ;; extra recepies
 (setq el-get-sources
       '(
