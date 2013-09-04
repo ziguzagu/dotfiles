c5fe2505c62ac6930a3ab74e5ca54671726becc9
ziguzagu <ziguzagu@gmail.com>
2013-09-04 15:26:18 +0900

    highlight trailing spaces.

--- .emacs.d/init-appearance.el
+++ .emacs.d/init-appearance.el
@@ -86,55 +86,6 @@
  '(diff-added       ((t (:background "darkolivegreen3" :foreground "black"))))
  '(diff-removed     ((t (:background "tomato" :foreground "black")))))
 
-;; setup color of zenkaku space, leading/trailing space and tab
-;;  * http://homepage3.nifty.com/satomii/software/jaspace.el
-(when (require 'jaspace nil t)
-  (when (boundp 'jaspace-modes)
-    (setq jaspace-modes (append jaspace-modes
-                                (list 'cperl-mode
-                                      'js2-mode
-                                      'css-mode
-                                      'html-helper-mode
-                                      'yaml-mode
-                                      'c-mode
-                                      'c++-mode
-                                      'text-mode
-                                      'default-generic-mode
-                                      'fundamental-mode))))
-  (when (boundp 'jaspace-alternate-jaspace-string)
-    (setq jaspace-alternate-jaspace-string "□"))
-  (when (boundp 'jaspace-highlight-tabs)
-    (setq jaspace-highlight-tabs ?^))
-  (add-hook 'jaspace-mode-off-hook
-            (lambda()
-              (when (boundp 'show-trailing-whitespace)
-                (setq show-trailing-whitespace nil))))
-  (add-hook 'jaspace-mode-hook
-            (lambda()
-              (progn
-                (when (boundp 'show-trailing-whitespace)
-                  (setq show-trailing-whitespace t))
-                (face-spec-set 'jaspace-highlight-jaspace-face
-                               '((((class color) (background light))
-                                  (:foreground "blue"))
-                                 (t (:foreground "green"))))
-                (face-spec-set 'jaspace-highlight-tab-face
-                               '((((class color) (background light))
-                                  (:foreground "red"
-                                               :background "unspecified"
-                                               :strike-through nil
-                                               :underline t))
-                                 (t (:foreground "magenta"
-                                                 :background "unspecified"
-                                                 :strike-through nil
-                                                 :underline t))))
-                (face-spec-set 'trailing-whitespace
-                               '((((class color) (background light))
-                                  (:foreground "red"
-                                               :background "unspecified"
-                                               :strike-through nil
-                                               :underline t))
-                                 (t (:foreground "magenta"
-                                                 :background "unspecified"
-                                                 :strike-through nil
-                                                 :underline t))))))))
+;; highlight trailing spaces
+(setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "gray16")
