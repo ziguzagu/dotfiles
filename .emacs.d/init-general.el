8c6c3a008286ded5749c836e70f20b71d7d8ac1c
ziguzagu <ziguzagu@gmail.com>
2014-02-13 14:36:59 +0900

    Fix an error at load init-general.el to set markdown mode faces. They should be set after loading markdown-mode.

--- .emacs.d/init-general.el
+++ .emacs.d/init-general.el
@@ -92,22 +92,24 @@
 (el-get 'sync 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-(set-face-attribute 'markdown-header-delimiter-face nil
-                    :foreground "orange")
-(set-face-attribute 'markdown-header-rule-face nil
-                    :foreground "orange")
-(set-face-attribute 'markdown-header-face nil
-                    :foreground "orange")
-(set-face-attribute 'markdown-inline-code-face nil
-                    :foreground "darkolivegreen3")
-(set-face-attribute 'markdown-pre-face nil
-                    :foreground "darkolivegreen3")
-(set-face-attribute 'markdown-language-keyword-face nil
-                    :foreground "gray52")
-(set-face-attribute 'markdown-list-face nil
-                    :foreground "mediumpurple1" :weight 'bold)
-(set-face-attribute 'markdown-link-face nil
-                    :foreground "color-75")
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (set-face-attribute 'markdown-header-delimiter-face nil
+                                :foreground "orange")
+            (set-face-attribute 'markdown-header-rule-face nil
+                                :foreground "orange")
+            (set-face-attribute 'markdown-header-face nil
+                                :foreground "orange")
+            (set-face-attribute 'markdown-inline-code-face nil
+                                :foreground "darkolivegreen3")
+            (set-face-attribute 'markdown-pre-face nil
+                                :foreground "darkolivegreen3")
+            (set-face-attribute 'markdown-language-keyword-face nil
+                                :foreground "gray52")
+            (set-face-attribute 'markdown-list-face nil
+                                :foreground "mediumpurple1" :weight 'bold)
+            (set-face-attribute 'markdown-link-face nil
+                                :foreground "color-75")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org
