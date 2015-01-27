358c86ee2782a5c6b688eca12d7f9b1fd4ff8455
ziguzagu <ziguzagu@gmail.com>
2015-01-27 23:34:47 +0900

    ad-dabbrev improve dabbrev by auto-complete!

--- .emacs.d/init-complete.el
+++ .emacs.d/init-complete.el
@@ -5,12 +5,13 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete-dict")
 (ac-config-default)
 
-;; dabbrev
-;(el-get 'sync 'dabbrev-highlight)
-;(require 'dabbrev-highlight)
-(setq dabbrev-case-fold-search t)
-(setq dabbrev-case-replace nil)
-(global-set-key "\C-o" 'dabbrev-expand)
+;; replace dabbrev by ac-dabbrev
+(require 'ac-dabbrev)
+(add-to-list 'ac-sources 'ac-source-dabbrev)
+(defun ac-dabbrev-expand ()
+  (interactive)
+  (auto-complete '(ac-source-dabbrev)))
+(global-set-key "\C-o" 'ac-dabbrev-expand)
 
 ;; yasnippet
 (setq yas-trigger-key "M-i")
