1e81d9a2776d6f507d2cd4704361569729dd09da
ziguzagu <ziguzagu@gmail.com>
2015-05-04 18:45:26 +0900

    Move

--- .emacs.d/init-coding.el
+++ .emacs.d/init-coding.el
@@ -14,3 +14,35 @@
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; sql-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'sql-mode-hook
+          (lambda ()
+            (sql-highlight-mysql-keywords)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; markdown-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (set-face-attribute 'markdown-header-delimiter-face nil
+                                :foreground "orange")
+            (set-face-attribute 'markdown-header-rule-face nil
+                                :foreground "orange")
+            (set-face-attribute 'markdown-header-face nil
+                                :foreground "orange")
+            (set-face-attribute 'markdown-inline-code-face nil
+                                :foreground "darkolivegreen3")
+            (set-face-attribute 'markdown-pre-face nil
+                                :foreground "darkolivegreen3")
+            (set-face-attribute 'markdown-language-keyword-face nil
+                                :foreground "gray52")
+            (set-face-attribute 'markdown-list-face nil
+                                :foreground "mediumpurple1" :weight 'bold)
+            (set-face-attribute 'markdown-link-face nil
+                                :foreground "color-75")))
