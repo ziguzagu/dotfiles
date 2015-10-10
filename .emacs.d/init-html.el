81aecf328522fdd7f0283ff49de9c1fbae796549
ziguzagu <ziguzagu@gmail.com>
2015-10-10 23:42:44 +0900

    Improve coffee mode indent

--- .emacs.d/init-html.el
+++ .emacs.d/init-html.el
@@ -66,7 +66,9 @@
 ;;;;;; coffee-mode
 (autoload 'coffee-mode "coffee-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(custom-set-variables '(coffee-tab-width 2))
+(custom-set-variables
+ '(coffee-tab-width 2)
+ '(coffee-indent-like-python-mode t))
 
 ;;;;;; utilities
 (defun escape-html-region (start end)
