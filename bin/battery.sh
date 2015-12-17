a8069035d5f34038fe2712d2766175cd54407d88
ziguzagu <ziguzagu@gmail.com>
2015-12-17 20:19:46 +0900

    Improve battery remaining display on tmux menu bar

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -x "/usr/bin/pmset" ]; then
-    /usr/bin/pmset -g ps | grep InternalBattery | awk '{print $2 "(" $4 ")"}' | sed 's/;//'
+    /usr/bin/pmset -g ps | grep InternalBattery | sed 's/[;()]//g' | perl -anE 'say "$F[1]($F[3])"'
 else
     echo -n ":D"
 fi
