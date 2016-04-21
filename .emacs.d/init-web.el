b5a5beb9428e7faaac0ecd88d6d2b2ed7e265f0a
ziguzagu <ziguzagu@gmail.com>
2016-04-21 09:43:02 +0900

    Open .html.* files with web-mode
    Like .html.ep, .html.erb and something else

--- .emacs.d/init-web.el
+++ .emacs.d/init-web.el
@@ -1,5 +1,6 @@
 (autoload 'web-mode "web-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\." . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tmpl$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tt$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tx$"   . web-mode))
