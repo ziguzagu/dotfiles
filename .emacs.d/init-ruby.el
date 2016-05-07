fae70f59eccb79e5680117f5f759dd4298982744
ziguzagu <ziguzagu@gmail.com>
2016-05-07 14:26:41 +0900

    Open Capfile, Gemfile and script including ruby in shebang

--- .emacs.d/init-ruby.el
+++ .emacs.d/init-ruby.el
@@ -1,3 +1,7 @@
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+
 (require 'ruby-end)
 (require 'ruby-block)
 (ruby-block-mode t)
