3978a4ac370a63b6e284bc561d07a64ba49d918b
ziguzagu <ziguzagu@gmail.com>
2016-05-08 11:33:46 +0900

    Setup ruby-omde by using use-package

--- .emacs.d/init-ruby.el
+++ .emacs.d/init-ruby.el
@@ -1,11 +1,21 @@
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+;;; init-ruby.el --- customize ruby mode
+;;; Commentary:
+;;; Code:
 
-(require 'ruby-end)
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle t)
+(use-package ruby-mode
+  :mode ("\\.rb$" "Capfile" "Gemfile")
+  :interpreter "ruby"
+  :config
+  (defun my-ruby-mode ()
+    (custom-set-variables
+     '(ruby-insert-encoding-magic-comment nil))
+    ;; complete end automatically
+    (require 'ruby-end)
+    ;; highlight begining of block cursor on the end
+    (require 'ruby-block)
+    (ruby-block-mode t)
+    (setq ruby-block-highlight-toggle t))
 
-(custom-set-variables
- '(ruby-insert-encoding-magic-comment nil))
+  (add-hook 'ruby-mode-hook 'my-ruby-mode))
+
+;;; init-ruby.el ends here
