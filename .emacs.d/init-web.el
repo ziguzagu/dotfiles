0e00a6af0c9be33f897463bd8213f5cf3aecb3df
ziguzagu <ziguzagu@gmail.com>
2016-05-08 14:48:12 +0900

    Refactoring init-web.el with use-package

--- .emacs.d/init-web.el
+++ .emacs.d/init-web.el
@@ -2,46 +2,26 @@
 ;;; Commentary:
 ;;; Code:
 
-(autoload 'web-mode "web-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.html" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tmpl$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tt$"   . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tx$"   . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(defun my-web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-style-padding 2)
-  (setq web-mode-script-padding 2)
-  (setq web-mode-engines-alist
-      '(("template-toolkit" . "\\.tt$")
-        ("template-toolkit" . "\\.tx$"))))
-(add-hook 'web-mode-hook  'my-web-mode-hook)
-
-;; JSX syntax highlighting
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-      (let ((web-mode-enable-part-face nil))
-        ad-do-it)
-    ad-do-it))
-;; flycheck by eslint in web-mode
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-;; disable jshint and json-jsonlist
-(setq-default flycheck-disabled-checkers
-              (append flycheck-disabled-checkers
-                      '(javascript-jshint)
-                      '(json-jsonlist)))
+(use-package web-mode
+  :mode ("\\.html" "\\.tmpl$" "\\.tt$" "\\.tx$")
+  :config
+  (defun my-web-mode-hook ()
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-style-padding 2)
+    (setq web-mode-script-padding 2)
+    (setq web-mode-engines-alist
+          '(("template-toolkit" . "\\.tt$")
+            ("template-toolkit" . "\\.tx$"))))
+  (add-hook 'web-mode-hook 'my-web-mode-hook))
 
 ;;;;;; css-mode
-(autoload 'scss-mode "scss-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.css$" . scss-mode))
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-(add-hook 'scss-mode-hook
-          (lambda ()
-            (setq css-indent-offset 2)
-            (setq scss-compile-at-save nil)))
+(use-package scss-mode
+  :mode ("\\.css$" "\\.scss$")
+  :config
+  (setq css-indent-offset 2)
+  (setq scss-compile-at-save nil))
 
 ;;;;;; js2-mode
 (use-package js2-mode
@@ -58,11 +38,12 @@
   (add-hook 'js2-mode-hook 'my-js2-mode))
 
 ;;;;;; coffee-mode
-(autoload 'coffee-mode "coffee-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(custom-set-variables
- '(coffee-tab-width 2)
- '(coffee-indent-like-python-mode t))
+(use-package coffee-mode
+  :mode "\\.coffee$"
+  :config
+  (custom-set-variables
+   '(coffee-tab-width 2)
+   '(coffee-indent-like-python-mode t)))
 
 ;;;;;; utilities
 (defun escape-html-region (start end)
