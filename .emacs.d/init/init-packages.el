f71e8a44bf1e58d012cefeb2c5edbab88e0361b3
ziguzagu <ziguzagu@gmail.com>
2016-10-13 12:45:37 +0900

    Cleanup

--- .emacs.d/init/init-packages.el
+++ .emacs.d/init/init-packages.el
@@ -1,11 +1,7 @@
 ;;; init-packages.el --- install packages
 ;;; Commentary:
 ;;; Code:
-(require 'package)
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(package-initialize)
 (package-refresh-contents)
 
 (package-install 'exec-path-from-shell)
