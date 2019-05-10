99b1898b66f9cc190d55b5b32c1f633c38d3bc6a
ziguzagu <ziguzagu@gmail.com>
2019-05-10 15:02:37 +0900

    Update indent of all shell script

--- bin/mute-off.sh
+++ bin/mute-off.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # mute-off.sh - Mute off volumn of MacOS X Lion.
 #
--- bin/mute-on.sh
+++ bin/mute-on.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # mute-on.sh - Mute on volumn of MacOS X Lion.
 #
