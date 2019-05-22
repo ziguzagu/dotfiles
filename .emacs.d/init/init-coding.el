9b818a9fd7728459b44429307d375685c09ba1c1
ziguzagu <ziguzagu@gmail.com>
2019-05-22 19:33:52 +0900

    Support `git add` and `git reset` via emacs vc mode

--- .emacs.d/init/init-coding.el
+++ .emacs.d/init/init-coding.el
@@ -18,6 +18,41 @@
       (shell-command (concat "git hub open " rev))))
   (define-key vc-annotate-mode-map (kbd "P") 'vc-annotate-open-pr-at-line))
 
+;; https://snarfed.org/emacs-vc-git-tweaks
+;;
+;; In vc-git and vc-dir for git buffers, make (C-x v) a run git add, u run git
+;; reset, and r run git reset and checkout from head.
+(defun my-vc-git-command (verb fn)
+  (let* ((fileset-arg (or vc-fileset (vc-deduce-fileset nil t)))
+         (backend (car fileset-arg))
+         (files (nth 1 fileset-arg)))
+    (if (eq backend 'Git)
+        (progn (funcall fn files)
+               (message (concat verb " " (number-to-string (length files))
+                                " file(s).")))
+      (message "Not in a vc git buffer."))))
+
+(defun my-vc-git-add (&optional revision vc-fileset comment)
+  (interactive "P")
+  (my-vc-git-command "Staged" 'vc-git-register))
+
+(defun my-vc-git-reset (&optional revision vc-fileset comment)
+  (interactive "P")
+  (my-vc-git-command "Unstaged"
+                     (lambda (files) (vc-git-command nil 0 files "reset" "-q" "--"))))
+
+(with-eval-after-load 'vc-git
+  (define-key vc-prefix-map [(r)] 'vc-revert-buffer)
+  (define-key vc-prefix-map [(a)] 'my-vc-git-add)
+  (define-key vc-prefix-map [(u)] 'my-vc-git-reset))
+(with-eval-after-load 'vc-dir
+  (define-key vc-dir-mode-map [(r)] 'vc-revert-buffer)
+  (define-key vc-dir-mode-map [(a)] 'my-vc-git-add)
+  (define-key vc-dir-mode-map [(u)] 'my-vc-git-reset)
+  ;; hide up to date files after refreshing in vc-dir
+  (define-key vc-dir-mode-map [(g)]
+    (lambda () (interactive) (vc-dir-refresh) (vc-dir-hide-up-to-date))))
+
 ;; flycheck
 (use-package flycheck
   :init
