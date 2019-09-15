da9f979099844991e5e6649a9e5c047532c5261b
ziguzagu <ziguzagu@gmail.com>
2019-09-15 23:31:09 +0900

    Setup org-capture to blogging with hugo

--- .emacs.d/init/init-org.el
+++ .emacs.d/init/init-org.el
@@ -7,12 +7,30 @@
   (setq org-startup-truncated nil)
   (setq org-startup-folded nil)
   (setq org-return-follows-link t)
+  (setq org-src-fontify-natively t) ;; hilight lines in code block
+
+  ;; blog template
+  (defun my/blog-subtree-post-capture-template ()
+    "Returns `org-capture' template string for new blog post."
+    (let ((section (format-time-string "posts/%Y/%m/" (org-current-time)))
+          (date (format-time-string "%Y-%m-%d" (org-current-time))))
+      (mapconcat 'identity
+                 `("** TODO %?"
+                   "  :PROPERTIES:"
+                   "  :EXPORT_FILE_NAME: "
+                   ,(concat "  :EXPORT_DATE: " date)
+                   ,(concat "  :EXPORT_HUGO_SECTION: " section)
+                   "  :END:"
+                   "\n")
+                 "\n")))
+
   ;; org-capture
   (setq org-capture-templates
         '(("m" "Memo" entry (file+datetree (concat org-directory "memo.org"))
-           "* [%<%H:%M>] %?\n"))
-  ;; hilight lines in code block
-  (setq org-src-fontify-natively t))
+           "* [%<%H:%M>] %?\n")
+          ("b" "Blog Posts" entry (file+olp "~/src/ziguzagu.org/blog.org" "Blog Posts")
+           (function my/blog-subtree-post-capture-template)
+           :prepend t :empty-lines 1))))
 
 (use-package ox-hugo
   :after ox)
