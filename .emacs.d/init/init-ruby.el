5c49e052aa7bb7a5a9a8819da3545bd16bf7c34f
ziguzagu <ziguzagu@gmail.com>
2019-12-10 23:08:29 +0900

    Improve projectile-rails keybind
    Invert downcase and uppercase in some command, because I need finding
    file from current file in almost case. So assign downcase key as a map
    to type easy.

--- .emacs.d/init/init-ruby.el
+++ .emacs.d/init/init-ruby.el
@@ -32,7 +32,13 @@
   (global-rbenv-mode))
 
 (use-package projectile-rails
-  :diminish projectile-rails-mode
+  :bind (:map projectile-rails-mode-map
+         ("C-c r m" . projectile-rails-find-current-model)
+         ("C-c r M" . projectile-rails-find-model)
+         ("C-c r c" . projectile-rails-find-current-controller)
+         ("C-c r C" . projectile-rails-find-controller)
+         ("C-c r s" . projectile-rails-find-current-spec)
+         ("C-c r S" . projectile-rails-find-spec))
   :config
   (projectile-rails-global-mode))
 
