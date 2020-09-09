8e0c5a45d5888d13c378e35b1c86a5273690bb8d
ziguzagu <ziguzagu@gmail.com>
2020-09-09 22:43:53 +0900

    Enable context-skk feature

--- .emacs.d/init/init-skk.el
+++ .emacs.d/init/init-skk.el
@@ -2,6 +2,9 @@
   :bind ("C-x C-j" . skk-mode)
   :init
   (setq skk-init-file "~/.config/ddskk/init")
+  (add-hook 'skk-load-hook
+            (lambda ()
+              (require 'context-skk)))
   :config
   (setq skk-server-host "localhost"
         skk-server-portnum 1178
