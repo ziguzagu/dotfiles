90ba56920d127019f5950156a4ce6a232a05e337
ziguzagu <ziguzagu@gmail.com>
2020-09-10 23:12:41 +0900

    Set ddskk as a default input method

--- .emacs.d/init/init-skk.el
+++ .emacs.d/init/init-skk.el
@@ -3,6 +3,7 @@
   :init
   (setq skk-init-file "~/.config/ddskk/init")
   (setq skk-user-directory "~/.local/share/ddskk")
+  (setq default-input-method "japanese-skk")
   (setq skk-preload t)
   (add-hook 'skk-load-hook
             (lambda ()
