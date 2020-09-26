7f279b122fc4dbfe967c057e6582828d9568ea93
ziguzagu <ziguzagu@gmail.com>
2020-09-26 22:36:50 +0900

    Rewrite settings of dired mode by use-package

--- .emacs.d/init/init-dired.el
+++ .emacs.d/init/init-dired.el
@@ -1,15 +1,19 @@
+(use-package dired
+  :ensure nil
+  :bind (:map dired-mode-map
+              ("RET" . dired-find-alternate-file))
+  :init
+  ;; use GNU ls installed by homebrew to use its own options, not have BSD ls.
+  (setq insert-directory-program "gls"
+        dired-listing-switches "-AlhXF --color=auto --group-directories-first")
+  ;; recursive copy/delete
+  (setq dired-recursive-copies 'always
+        dired-recursive-deletes 'always)
+  ;; don't create new buffer at moving direcotry
+  (put 'dired-find-alternate-file 'disabled nil))
+
 (require 'wdired)
 (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
-
-;; ls options
-(setq insert-directory-program "gls") ;; homebrew
-(setq dired-listing-switches "-AlhXF --color=auto --group-directories-first")
-;; recursive copy/delete
-(setq dired-recursive-copies 'always)
-(setq dired-recursive-deletes 'always)
-;; don't create new buffer at moving direcotry
-(put 'dired-find-alternate-file 'disabled nil)
-(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
 
 ;; uniqufy
 (require 'uniquify)
