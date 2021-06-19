dd12a4eda64a9de50a6fe10a7ffd37639fdeea44
ziguzagu <ziguzagu@gmail.com>
2021-06-19 23:59:46 +0900

    Merge init-ruby.el into init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -578,9 +578,43 @@
   (go-mode . lsp-deferred)
   (go-mode . lsp-go-install-save-hooks))
 
+(use-package ruby-mode
+  :interpreter "ruby"
+  :hook
+  (ruby-mode . lsp-deferred)
+  :custom
+  (ruby-insert-encoding-magic-comment nil))
+
+(use-package ruby-end
+  :hook
+  (ruby-mode . ruby-end-mode))
+
+(use-package rspec-mode
+  :custom
+  (rspec-use-docker-when-possible t)
+  (rspec-use-relative-path t)
+  :config
+  (rspec-install-snippets))
+
+(use-package inf-ruby
+  :hook
+  ((ruby-mode . inf-ruby-minor-mode)
+   (ruby-mode . inf-ruby-switch-setup)))
+
+(use-package rbenv
+  :config
+  (global-rbenv-mode))
+
+(use-package projectile-rails
+  :bind-keymap
+  ("C-c r" . projectile-rails-command-map)
+  :config
+  (projectile-rails-global-mode))
+
+(use-package haml-mode)
+
 (add-to-list 'load-path "~/.emacs.d/init")
 ;(load "init-helm")
-(load "init-ruby")
 (load "init-perl")
 (load "init-javascript")
 (load "init-web")
