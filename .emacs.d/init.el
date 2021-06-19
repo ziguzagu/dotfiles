875f0e878e067de926a6acf2ab49cdbc21380390
ziguzagu <ziguzagu@gmail.com>
2021-06-20 00:02:27 +0900

    Merge init-javascript.el into init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -686,9 +686,31 @@
             (flycheck-mode t)
             (setq flycheck-checker 'perl-project-libs)))
 
+(use-package js2-mode
+  :mode (("\\.js\\'"   . js2-mode)
+         ("\\.jsx\\'"  . js2-jsx-mode))
+  :interpreter ("node" . js2-mode)
+  :custom
+  (js2-basic-offset 2))
+
+(use-package json-mode)
+
+(use-package typescript-mode
+  :custom
+  (typescript-indent-level 2))
+
+(use-package coffee-mode
+  :mode "\\.coffee\\'"
+  :custom
+  (coffee-tab-width 2)
+  (coffee-indent-like-python-mode t))
+
+(use-package prettier-js
+  :hook
+  (js2-mode . prettier-js))
+
 (add-to-list 'load-path "~/.emacs.d/init")
 ;(load "init-helm")
-(load "init-javascript")
 (load "init-web")
 (load "init-terraform")
 (load "init-docker")
