a2e1d6af1fd1a88b7631859c622be86fa6816eb8
ziguzagu <ziguzagu@gmail.com>
2021-06-19 23:49:34 +0900

    Merge init-completion.el into init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -367,8 +367,21 @@
   :config
   (company-statistics-mode))
 
+(use-package vertico
+  :pin melpa-stable
+  :config
+  (vertico-mode))
+
+(use-package consult
+  :pin melpa-stable
+  :bind (("C-c s" . consult-imenu)))
+
+(use-package orderless
+  :pin melpa-stable
+  :custom
+  (completion-styles '(orderless)))
+
 (add-to-list 'load-path "~/.emacs.d/init")
-(load "init-completion")
 (load "init-helm")
 (load "init-coding")
 (load "init-flycheck")
