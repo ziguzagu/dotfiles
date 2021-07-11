8ce37393cd51b62fde496c565d464bf31a3006da
ziguzagu <ziguzagu@gmail.com>
2021-07-11 23:12:46 +0900

    Bind `C-=` to go-mode local map

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -591,22 +591,22 @@
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 
-(defun my:insert-short-var-declaration-op ()
-  "Insert `:=` at the point"
-  (interactive)
-  (unless (string= (string (char-before)) " ")
-    (insert " "))
-  (insert ":=")
-  (unless (string= (string (char-after)) " ")
-    (insert " ")))
-
 (use-package go-mode
-  :bind
-  ;; map `C-=` to default ascii codes `^[[61;5u` by iTerm's key map
-  (("C-=" . my:insert-short-var-declaration-op))
+  :bind (:map go-mode-map
+         ;; map `C-=` to default ascii codes `^[[61;5u` by iTerm's key map
+         ("C-=" . my:insert-short-var-declaration-op))
   :hook
   (go-mode . lsp-deferred)
-  (go-mode . my:lsp-go-install-save-hooks))
+  (go-mode . my:lsp-go-install-save-hooks)
+  :config
+  (defun my:insert-short-var-declaration-op ()
+    "Insert `:=` at the point"
+    (interactive)
+    (unless (string= (string (char-before)) " ")
+      (insert " "))
+    (insert ":=")
+    (unless (string= (string (char-after)) " ")
+      (insert " "))))
 
 (use-package ruby-mode
   :interpreter "ruby"
