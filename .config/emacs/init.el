307332a70f41bb7e83413d7b716b9f9e25398abf
ziguzagu <ziguzagu@gmail.com>
2022-10-23 22:56:59 +0900

    Remove flycheck with Project::Libs
    It should be probably replaced by Perl::LanguageServer.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -703,26 +703,6 @@
 
 (use-package plenv)
 
-;; flycheck with Project::Libs
-(flycheck-define-checker perl-project-libs
-  "A perl syntax checker with Project::Libs."
-  :command ("plenv"
-            "exec"
-            "perl"
-            "-MProject::Libs lib_dirs => [qw(local/lib/perl5)]"
-            "-wc"
-            source-inplace)
-  :error-patterns ((error line-start
-                          (minimal-match (message)) " at " (file-name) " line " line
-                          (or "." (and ", " (zero-or-more not-newline)))
-                          line-end))
-  :modes (cperl-mode))
-
-(add-hook 'cperl-mode-hook
-          (lambda ()
-            (flycheck-mode t)
-            (setq flycheck-checker 'perl-project-libs)))
-
 (use-package js2-mode
   :ensure t
   :mode (("\\.js\\'"  . js2-mode)
