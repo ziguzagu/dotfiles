ce590ecdc74db35ac85ea507006b69896ff9ee9f
ziguzagu <ziguzagu@gmail.com>
2022-11-03 12:15:19 +0900

    Remove coffee-mode

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -721,12 +721,6 @@
   :custom
   (typescript-indent-level 2))
 
-(use-package coffee-mode
-  :mode "\\.coffee\\'"
-  :custom
-  (coffee-tab-width 2)
-  (coffee-indent-like-python-mode t))
-
 (use-package prettier-js
   :hook
   (js2-mode . prettier-js))
