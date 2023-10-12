80470e0e0b51f0330383ba48debffbce002e527d
ziguzagu <ziguzagu@gmail.com>
2023-10-12 23:26:51 +0900

    Use lsp-docker

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -615,6 +615,8 @@
   ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#increase-the-amount-of-data-which-emacs-reads-from-the-process
   (setq read-process-output-max (* 1024 1024)))
 
+(use-package lsp-docker)
+
 (use-package consult-lsp
   :ensure t)
 
