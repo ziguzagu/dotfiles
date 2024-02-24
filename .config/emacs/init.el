61308457ed54d374aa2f8a1a3fe3b4143647bb28
ziguzagu <ziguzagu@gmail.com>
2024-02-24 18:19:43 +0900

    Improve web-mode indentations for JSX

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -787,11 +787,14 @@
 
 (use-package web-mode
   :ensure t
-  :mode ("\\.tsx\\'" "\\.html\\'" "\\.tmpl\\'" "\\.tt\\'" "\\.tx\\'")
+  :mode ("\\.jsx\\'" "\\.tsx\\'" "\\.html\\'" "\\.tmpl\\'" "\\.tt\\'" "\\.tx\\'")
   :custom
+  (web-mode-attr-indent-offset nil)
   (web-mode-markup-indent-offset 2)
   (web-mode-css-indent-offset 2)
   (web-mode-code-indent-offset 2)
+  (indent-tabs-mode nil)
+  (tab-width 2)
   (web-mode-style-padding 2)
   (web-mode-script-padding 2)
   (web-mode-engines-alist '(("template-toolkit" . "\\.tt\\'")
