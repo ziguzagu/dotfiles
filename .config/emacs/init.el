195501aec9682a424f88827dca8856c06c8c27cf
ziguzagu <ziguzagu@gmail.com>
2025-03-16 22:41:38 +0900

    Make C-g a bit more helpful

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -70,11 +70,38 @@
       (when bounds
         (kill-region (car bounds) (cdr bounds)))))
 
+  ;; Make C-g a bit more helpful
+  ;; https://protesilaos.com/codelog/2024-11-28-basic-emacs-configuration/
+  (defun my:keyboard-quit-dwim ()
+    "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+    The generic `keyboard-quit' does not do the expected thing when
+    the minibuffer is open.  Whereas we want it to close the
+    minibuffer, even without explicitly focusing it.
+
+    The DWIM behaviour of this command is as follows:
+
+    - When the region is active, disable it.
+    - When a minibuffer is open, but not focused, close the minibuffer.
+    - When the Completions buffer is selected, close it.
+    - In every other case use the regular `keyboard-quit'."
+    (interactive)
+    (cond
+     ((region-active-p)
+      (keyboard-quit))
+     ((derived-mode-p 'completion-list-mode)
+      (delete-completion-window))
+     ((> (minibuffer-depth) 0)
+      (abort-recursive-edit))
+     (t
+      (keyboard-quit))))
+
   :bind (("RET" . newline-and-indent)
          ("C-M-r" . isearch-backward)
          ("C-M-s" . isearch-forward)
          ("C-c ]" . align-regexp)
          ("C-c h" . help-for-help)
+         ("C-g" . my:keyboard-quit-dwim)
          ("C-h" . delete-backward-char)
          ("C-r" . isearch-backward-regexp)
          ("C-s" . isearch-forward-regexp)
